7ab054af4af27c2f4a8befa7b1c27cbd611c5789
IT24103959 <IT24103959@my.sliit.lk>
2025-10-03 13:12:20 +0530

    boilerplate

---
+++ adspark/src/test/java/com/service/adspark/AdsparkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.adspark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsparkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
